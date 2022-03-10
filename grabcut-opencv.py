d1b542eaceb33b11231bf963cb474a60162a179f
alaminkawsar <alaminkawsar8643@gmail.com>
2022-03-10 21:27:29 +0600

    new file added

--- grabcut-opencv.py
+++ grabcut-opencv.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2 as cv 
 from matplotlib import pyplot as plt
 
-img = cv.imread('input.jpg')
+img = cv.imread('Hasib.jpg')
 mask = np.zeros(img.shape[:2], np.uint8)
 
 # color no change
@@ -12,14 +12,14 @@ img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 bgdModel = np.zeros((1,65), np.float64)
 fgdModel = np.zeros((1,65), np.float64)
 
-rect = (160, 0, 110, 400)
+rect = (300, 550, 240, 1200)
 cv.grabCut(img, mask,rect, bgdModel, fgdModel, 5, cv.GC_INIT_WITH_RECT)
 
 mask2 = np.where((mask==2)|(mask==0), 0, 1).astype('uint8')
 img = img*mask2[:,:,np.newaxis]
 
 # newmask is the mask image I manually labelled
-newmask = cv.imread('draw.jpg',0)
+newmask = cv.imread('mark.jpg',0)
 # wherever it is marked white (sure foreground), change mask=1
 # wherever it is marked black (sure background), change mask=0
 mask[newmask == 0] = 0
